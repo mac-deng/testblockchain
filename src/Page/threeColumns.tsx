76fe11f7e5ab0af2347994574126cb7e71026417
Mike Deng <hjqdengwei@gmail.com>
2022-02-23 21:28:59 +0800

    修改文案

--- src/Page/threeColumns.tsx
+++ src/Page/threeColumns.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ThreeColumns() {
   return (<div>
-  		<div className="tip">放大或者缩小显示器看效果</div>
+  		<div className="tip">放大或者缩小浏览器看效果</div>
   		<div className="threeColumns">
 		  <div className='box1'></div>
 		  <div className='box2'></div>
